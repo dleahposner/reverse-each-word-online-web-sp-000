e85710e2ef55084de8f90dc0d062b806fbae9330
Devorah Posner <dleahposner@gmail.com>
2021-02-04 21:35:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,19 @@
+<<<<<<< HEAD
+=======
+# def reverse_each_word(sentence1)
+#   sentence1.collect do |words|
+#     words.reverse
+#   end
+# end
+>>>>>>> da15e180699bcc95e059dfc5aab44a44de17577d
 
 def reverse_each_word(sentence1)
   sentence1.split.collect do |joined_array|
     joined_array.reverse
+<<<<<<< HEAD
    end.join(' ')
+=======
+   end
+  # .join(' ')
+>>>>>>> da15e180699bcc95e059dfc5aab44a44de17577d
 end
